a0859b0555434a840a6778e60457e2de4580fedd
prashant <prneje@gmail.com>
2024-01-07 11:14:08 +0530

    update config server

---
+++ elastic/elastic-config/src/main/java/com/microservices/demo/elastic/config/ElasticsearchConfig.java
@@ -0,0 +1,25 @@
+package com.microservices.demo.elastic.config;
+
+import com.microservices.demo.config.ElasticConfigData;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+    private final ElasticConfigData elasticConfigData;
+
+    public ElasticsearchConfig(ElasticConfigData configData) {
+        this.elasticConfigData = configData;
+    }
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(elasticConfigData.getConnectionUrl())
+                .withConnectTimeout(elasticConfigData.getConnectTimeoutMs())
+                .withSocketTimeout(elasticConfigData.getSocketTimeoutMs())
+                .build();
+    }
+}
